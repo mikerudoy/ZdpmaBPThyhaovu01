1fbd9666ef15c0cfcff90b5bcb7d0ff48d7a9c41
Mike <mike.rudoi@gmail.com>
2024-05-01 19:26:39 +0100

    Initial commit

---
+++ SwiftUIMarathon01/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  SwiftUIMarathon01
+//
+//  Created by Mike Rudoy on 01/05/2024.
+//
+
+import SwiftUI
+
+struct FirstView: View {
+    
+    private enum Constants {
+        static let rectangleHeight: CGFloat = 50
+    }
+    
+    private let data: Range<Int> = 1..<101
+    
+    var body: some View {
+        TabView {
+            VStack {
+                ScrollView {
+                    VStack {
+                        ForEach(data, id: \.self) { item in
+                            Text("\(item)")
+                                .font(.title.monospacedDigit())
+                                .foregroundStyle(Color(uiColor: .systemGray3))
+                                .frame(maxWidth: .infinity)
+                        }
+                    }
+                }
+                .scrollClipDisabled()
+                    Rectangle()
+                        .fill(Color.red.opacity(0.8))
+                        .frame(height: Constants.rectangleHeight)
+            }.tabItem {
+                Label("First", systemImage: "star")
+            }
+        }
+    }
+}
+
+#Preview {
+    FirstView()
+}
---
+++ SwiftUIMarathon01/SwiftUIMarathon01App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMarathon01App.swift
+//  SwiftUIMarathon01
+//
+//  Created by Mike Rudoy on 01/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMarathon01App: App {
+    var body: some Scene {
+        WindowGroup {
+            FirstView()
+        }
+    }
+}
